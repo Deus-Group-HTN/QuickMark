6989cccc45467fb57b77143cdb29dd5cc92d6eb7
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2021-01-16 17:00:04 -0500

    made python more modular

--- python/main.py
+++ python/main.py
@@ -2,13 +2,15 @@ from pdf2image import convert_from_path
 import cv2
 import numpy as np
 import os
-images = convert_from_path('Hack the North Test Test - Filled.pdf')
+import sys
+
+
+images = convert_from_path('../public/temp/' + sys.argv[1])
 img =  cv2.cvtColor(np.array(images[0]), cv2.COLOR_RGB2BGR)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
 thresh_inv = cv2.inRange(gray, 0, 120)
-#thresh_inv = cv2.bitwise_not(cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 1))
 
 # Blur the image
 blur = cv2.GaussianBlur(thresh_inv,(1,1),0)
@@ -20,7 +22,7 @@ thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
 # find contours
 contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-mask = np.ones(img.shape[:2], dtype="uint8") * 255
+# mask = np.ones(img.shape[:2], dtype="uint8") * 255
 box_tuples = []
 for c in contours:
     # get the bounding rect
@@ -33,13 +35,13 @@ for box in box_tuples:
     question_number += 1
     x, y, w, h = box
     crop_img = img[y:y+h, x:x+w]
-    filename = 'questions/' + str(question_number) + '.jpg'
+    filename = '../public/questions/' + sys.argv[2] + str(question_number) + '.jpg'
     cv2.imwrite(os.path.join(os.getcwd(), filename), crop_img)
     cv2.rectangle(mask, (x, y), (x+w, y+h), (0, 0, 255), -1)
 
-res_final = cv2.bitwise_and(img, img, mask=cv2.bitwise_not(mask))
+# res_final = cv2.bitwise_and(img, img, mask=cv2.bitwise_not(mask))
 
-cv2.imshow("boxes", mask)
-cv2.imshow("final image", res_final)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("boxes", mask)
+# cv2.imshow("final image", res_final)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
